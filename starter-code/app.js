313e3528844f40ec68f1cdecbb8dcf8347574eeb
LAURA MARCOS AGUILAR <Laura@MacBook-Pro-de-LAURA.local>
2018-01-09 21:24:04 +0100

    iteracion 1 y algo de la 2

--- starter-code/app.js
+++ starter-code/app.js
@@ -3,3 +3,15 @@ const app = express();
 const Chuck  = require('chucknorris-io');
 const client = new Chuck();
 
+// Retrieve a random chuck joke
+app.get('/random', (request, response, next) => {
+  client.getRandomJoke()
+  .then((response) => {
+    console.log("chuck");
+    // use the response here
+  }).catch((err) => {
+    // handle error
+  })
+});
+
+app.listen(4000);
